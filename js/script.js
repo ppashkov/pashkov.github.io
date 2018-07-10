a590ee8e680da7d60654b603427af96aa10f4e91
ppashkov <pashkov.wd@gmail.com>
2018-07-11 02:58:55 +0300

    day_night

--- js/script.js
+++ js/script.js
@@ -17,3 +17,47 @@
   $(function() {
    $("#go-top").scrollToTop();
   });
+
+
+  $(".day").click(function(){
+    $(".s1bg").css("background-image", "url(img/bg2.jpg)");
+    $(".me").animate({
+      backgroundColor: "rgba(180,180,180,0.60)",
+    }, 600 );
+    $(".skills").animate({
+      backgroundColor: "rgba(150,150,150,0.60)",
+    }, 600 );
+    $(".wtl").animate({
+      backgroundColor: "rgba(200,200,200,0.60)",
+    }, 600 );
+    $(".content").animate({
+      backgroundColor: "rgba(160,160,160,0.75)",
+    }, 600 );
+    $(".txt").animate({
+      color: "black",
+    }, 600);
+    $(".ghlink").css("filter","invert(0)")
+    
+  });
+  $(".night").click(function(){
+    
+    
+    $(".me").animate({
+      backgroundColor: "rgba(20,20,20,0.80)",
+    }, 600 );
+    $(".skills").animate({
+      backgroundColor: "rgba(12,12,12,0.80)",
+    }, 600 );
+    $(".wtl").animate({
+      backgroundColor: "rgba(25,25,25,0.80)",
+    }, 600 );
+    $(".content").animate({
+      backgroundColor: "rgba(20,20,20,0.85)",
+    }, 600 );
+    $(".txt").animate({
+      color: "white",
+    }, 600);
+    $(".ghlink").css("filter","invert(1)")
+    $(".s1bg").css("background-image", "url(img/bg.jpg)");
+    
+  });
